73bb3dcfff9d64876ce21b9cd29440acd4b43d4e
IdealistCat <113909588+IdealistCat@users.noreply.github.com>
2024-10-24 14:52:59 +0000

    set identifier

--- src/webscript/elements.js
+++ src/webscript/elements.js
@@ -2,6 +2,7 @@ export function createParagraph({content="",id=""}){
     let paragraph = document.createElement('p');
 
     paragraph.innerHTML = content;
+    paragraph.id = id;
 
     return paragraph;
 }
