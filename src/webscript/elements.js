c826ac4651877d892188bd8cfea583c368252936
IdealistCat <113909588+IdealistCat@users.noreply.github.com>
2024-10-24 14:52:34 +0000

    paragraph var and return

--- src/webscript/elements.js
+++ src/webscript/elements.js
@@ -1,1 +1,5 @@
-export function createParagraph({content="",id=""}) {}
+export function createParagraph({content="",id=""}){
+    let paragraph = document.createElement('p');
+    
+    return paragraph;
+}
